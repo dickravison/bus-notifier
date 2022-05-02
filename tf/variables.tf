1149a838d5ee8e542ef2e3a51837f2c194a29510
Rick <rick@dvsn.io>
2022-05-02 12:46:40 +0100

    Adding descriptions to variables

--- tf/variables.tf
+++ tf/variables.tf
@@ -1,7 +1,27 @@
-variable "region" {}
-variable "project_name" {}
-variable "layer_name" {}
-variable "telegram_chat_id" {}
-variable "telegram_token" {}
-variable "hometowork" {}
-variable "worktohome" {}
+variable "region" {
+  description = "AWS region to deploy the application in"
+}
+
+variable "project_name" {
+  description = "Name of the project for this application"
+}
+
+variable "layer_name" {
+  description = "Name of existing lambda layer to use for application dependencies"
+}
+
+variable "telegram_chat_id" {
+  description = "ID of the telegram chat to post notifications to"
+}
+
+variable "telegram_token" {
+  description = "Token for the telegram chat to post notifications to"
+}
+
+variable "hometowork" {
+  description = "URL for the home to work bus stop"
+}
+
+variable "worktohome" {
+  description = "URL for the work to home bus stop"
+}
