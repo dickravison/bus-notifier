1c0e76751ec2a2026dd27cc3cbed3108d0c20b6b
Rick <rick@dvsn.io>
2022-05-02 12:48:11 +0100

    Adding type for variables and formatting terraform

--- tf/lambda.tf
+++ tf/lambda.tf
@@ -19,21 +19,21 @@ EOF
 }
 
 resource "aws_lambda_function" "bus_notifier" {
-  filename      = "../src/lambda.zip"
-  function_name = "bus-notifier"
-  role          = aws_iam_role.iam_for_lambda.arn
-  handler       = "main.scrape"
+  filename         = "../src/lambda.zip"
+  function_name    = "bus-notifier"
+  role             = aws_iam_role.iam_for_lambda.arn
+  handler          = "main.scrape"
   source_code_hash = filebase64sha256("../src/lambda.zip")
-  runtime = "python3.9"
-  layers = [data.aws_lambda_layer_version.layer.arn]
-  architectures = ["arm64"]
+  runtime          = "python3.9"
+  layers           = [data.aws_lambda_layer_version.layer.arn]
+  architectures    = ["arm64"]
 
   environment {
     variables = {
       TELEGRAM_CHAT_ID = var.telegram_chat_id
-      TELEGRAM_TOKEN = var.telegram_token
-      HOMETOWORK = var.hometowork
-      WORKTOHOME = var.worktohome
+      TELEGRAM_TOKEN   = var.telegram_token
+      HOMETOWORK       = var.hometowork
+      WORKTOHOME       = var.worktohome
     }
   }
 
--- tf/variables.tf
+++ tf/variables.tf
@@ -1,27 +1,34 @@
 variable "region" {
+  type        = string
   description = "AWS region to deploy the application in"
 }
 
 variable "project_name" {
+  type        = string
   description = "Name of the project for this application"
 }
 
 variable "layer_name" {
+  type        = string
   description = "Name of existing lambda layer to use for application dependencies"
 }
 
 variable "telegram_chat_id" {
+  type        = string
   description = "ID of the telegram chat to post notifications to"
 }
 
 variable "telegram_token" {
+  type        = string
   description = "Token for the telegram chat to post notifications to"
 }
 
 variable "hometowork" {
+  type        = string
   description = "URL for the home to work bus stop"
 }
 
 variable "worktohome" {
+  type        = string
   description = "URL for the work to home bus stop"
 }
